e64eab92d5f569db94e9a7d1ba6e2a555fd28d00
Pantaiz <pantaiz@yandex.ru>
2023-02-17 16:08:34 +0300

    Домашнее задание N4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeChecked && onChangeChecked(e.currentTarget.checked)
+        onChangeChecked?.(e.currentTarget.checked)
+        onChange?.(e)
 
     }
 
